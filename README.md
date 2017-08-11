1c5d26402fbfa12fc9113d55e90be9d4d69a39d2
Katrina Kalantar <kkalantar@berkeley.edu>
2017-08-10 21:19:50 -0700

    adding lzw_compress.py to remove low complexity sequences

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-#
-PhageDisplayLib
+#PhageDisplayLib
+
+###Agilent_Order.py
 
 Adapted from bodonovan's original script.
 Generate a .fasta and .txt sequence file containing the agilent order for peptidome.
 
 ```
 python Agilent_Order.py --help
-
 ```
 
 
@@ -16,7 +16,19 @@ To generate the agilent order for 62-AA sequences with 31-AA window and STREP an
 
 ```
 python Agilent_Order.py -i input_file.fasta -o output_filename -k 62 -w 31 -fp AGCCATCCGCAGTTCGAGAAA -tp GACTACAAGGACGACGATGAT
+```
 
+
+###lzw_compress.py
+
+Dependencies:
+pip install lzstring
+
+Compress each line in .fasta file to obtain a compression ratio. If ratio < threshold, filter out the sequence.
+Returns a .fasta file with low complexity sequences removed.
+
+```
+python lzw_compress.py -i input_file.fasta -o output_filename -t .5
 ```
 
 
---
+++ lzw_compress.py
@@ -0,0 +1,52 @@
+import argparse
+import lzstring
+
+### create parser object ###
+parser = argparse.ArgumentParser(description = 'Generate agilent order')
+parser.add_argument('-i','-infile', help='input filepath',type=str)
+parser.add_argument('-o','-outfile', help='output filepath',type=str)
+parser.add_argument('-t','-threshold', help='compression ratio threshold; default = .5', default=.5, nargs='?', type=float)
+args = parser.parse_args()
+
+
+### utility functions ###
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
+def get_compression_ratio(seq):
+    x = lzstring.LZString()
+    compressed = x.compressToBase64(seq)
+    ratio = utf8len(compressed) / utf8len(seq)
+    return ratio
+
+### initialize variables ###
+header = ''
+seq = ''
+
+outfile = open(args.o + '.fasta', 'w')
+
+### main ###
+
+with open(args.i,'r') as f:  #open the input file
+
+    for line in f:
+
+        if '>' in line:
+            if len(seq) > 0:
+                cr = get_compression_ratio(seq)
+                if cr > args.t:  #if compression ratio > the specified threshold
+                    outfile.write(header + '\n')
+                    outfile.write(seq + '\n')
+
+            seq = ''     #reset sequence
+            header = line.strip()
+
+        else:     #this is not a header line
+            seq += line.strip()
+
+    #finish the remaining sequence after last header
+    if len(seq) > 0:
+        cr = get_compression_ratio(seq)
+        if cr > args.t:  #if compression ratio > the specified threshold
+            outfile.write(header + '\n')
+            outfile.write(seq + '\n')
